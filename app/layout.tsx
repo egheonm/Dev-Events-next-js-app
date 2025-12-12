a6a1706e51e0f7437d668fff4f625bbae47b6be4
Egheomwanre <egheomwanre7@gmail.com>
2025-12-12 17:56:16 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from '@/Lights/LightRays'
+import NavBar from '@/components/NavBar'
+const SchibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const MartianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The Hub for Every Dev Event You Musn't Miss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${SchibstedGrotesk.variable} ${MartianMono.variable} min-h-screen antialiased`}
+      >
+        <NavBar />
+        <div className="absolute top-0 inset-0 z-[-1] min-h-screen">
+
+
+
+  <LightRays
+    raysOrigin="top-center-offset"
+    raysColor="#5dfeca"
+    raysSpeed={0.7}
+    lightSpread={0.9}
+    rayLength={3}
+    followMouse={true}
+    mouseInfluence={0.005}
+    noiseAmount={0.0}
+    distortion={0.015}
+  />
+
+        </div>
+        <main>{children}</main>
+        
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import EventCard from '@/components/EventCard'
+import ExploreBtn from'@/components/ExploreBtn'
+import {events} from "@/lib/constants"
+
+interface Props {
+    title : string,
+    image : string,
+    slug : string,
+    location : string,
+    date : string,
+    time : string,
+}
+
+const Home = () => {
+  return (
+    <section>
+    <h1 className='text-center'>The Hub for Every Dev <br /> Event You Can't Miss</h1>
+    <p className='mt-5 text-center'>Hackathons, Meetups and Conferences, All in One Place</p>
+
+    <ExploreBtn />
+
+    <div className='space-y-7 mt-20'> 
+      <h3>Featured Event</h3>
+
+      <ul className="events">
+        {events.map((event : Props) => (
+          <li key={event.title}>
+            <EventCard  {...event}/>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </section>
+  )
+}
+
+export default Home
